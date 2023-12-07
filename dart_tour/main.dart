fa1973b5e912b897e5241003e12eb26c18afc7f6
Euijin Song <lstar2397@naver.com>
2023-12-08 03:53:55 +0900

    #1.4 Final Variables

--- dart_tour/main.dart
+++ dart_tour/main.dart
@@ -1,7 +1,4 @@
 void main() {
-    String name = 'euijin';
-    name = null; // Error: A value of type 'Null' can't be assigned to a variable of type 'String'.
-    
-    String? nullableName = 'euijin';
-    nullableName = null;
+    final name = 'euijin';
+    name = 'nico';
 }
