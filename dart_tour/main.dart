faf95e1b96abf7262e929ac5bda2788025ab289a
Euijin Song <lstar2397@naver.com>
2023-12-08 22:35:04 +0900

    #4.2 Named Constructor Parameters

--- dart_tour/main.dart
+++ dart_tour/main.dart
@@ -1,8 +1,15 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 
   void sayHello() {
     print("Hi my name is $name");
@@ -10,8 +17,16 @@ class Player {
 }
 
 void main() {
-  var player = Player("euijin", 1500);
-  player.sayHello();
-  var player2 = Player("lynn", 2500);
-  player2.sayHello();
+  var player = Player(
+    name: "euijin",
+    xp: 1500,
+    team: "red",
+    age: 12,
+  );
+  var player2 = Player(
+    name: "lynn",
+    xp: 2500,
+    team: "blue",
+    age: 12,
+  );
 }
