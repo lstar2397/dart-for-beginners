fc80a21bf87eb4ca5b6b1e412e1d0a0515a9b730
Euijin Song <lstar2397@naver.com>
2023-12-08 09:31:39 +0900

    #3.3 Optional Positional Parameters

--- dart_tour/main.dart
+++ dart_tour/main.dart
@@ -1,15 +1,6 @@
-String sayHello({
-  required String name,
-  required int age,
-  required String country,
-}) {
-  return "Hello $name, you are $age, and you come from $country";
-}
+String sayHello(String name, int age, [String country = "cuba"]) =>
+    "Hello $name, you are $age, and you come from $country";
 
 void main() {
-  print(sayHello(
-    name: "euijin",
-    age: 23,
-    country: "South Korea",
-  ));
+  print(sayHello("euijin", 23));
 }
